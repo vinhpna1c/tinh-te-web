46f43c679e80e7678f2e6dabeb6841943b8bda3a
vinhpna1c <114041994+vinhpna1c@users.noreply.github.com>
2023-05-25 17:33:25 +0700

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Header from '../components/home/Header'
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <p>Home page</p>
+    </div>
+  )
+}
