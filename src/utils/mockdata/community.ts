737613587dabcd958ebaaf234e5f02e66012bca8
vinhpna1c <114041994+vinhpna1c@users.noreply.github.com>
2023-06-15 10:19:50 +0700

    update for deploy

---
+++ src/utils/mockdata/community.ts
@@ -0,0 +1,22 @@
+export const tempDataCommunityItem = [
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/335jFM98Ee_UTetCqLS9cDb7qrO8JLJ8tKpKTHPVOU0/h:144/plain/https://photo2.tinhte.vn/data/attachment-files/2023/04/6399434_og.jpg",
+      badge: "STMN23",
+      itemnumber: "0"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/FzbB8WbC5RRCJmjcpXwJ-jTD-k8zgoRExnHTStu7Khc/h:144/plain/https://photo2.tinhte.vn/data/attachment-files/2023/04/6404614_og.jpg",
+      badge: "STMN24",
+      itemnumber: "0"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/SwYdQxsUY_qhucBfBsoZD0CPs62mXNvXfNIhIsfhU18/h:144/plain/https://photo2.tinhte.vn/data/attachment-files/2022/06/6015223_og.jpg",
+      badge: "STMN25",
+      itemnumber: "0"
+    },
+    {
+      imgURL: "https://imgproxy4.tinhte.vn/_BGHezAUloWC-4ROA6WbZoQSbKyy0ms_37DdzPYOG40/h:144/plain/https://s3.cloud.cmctelecom.vn/tinhte1/2018/10/4454647_og.png",
+      badge: "STMN26",
+      itemnumber: "0"
+    }
+  ]
--- src/utils/mockdata/post.ts
+++ src/utils/mockdata/post.ts
@@ -7,6 +7,245 @@ export const topPosts=[
     }
 ];
 
+export const tempDataComputexItem = [
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6462416_Cover-Apple-vision-pro-co-the-phai-doi-ten-o-trung-quoc-tinhte-tuanhtran.jpg",
+      title: "Apple Vision Pro có thể phải đổi tên tại thị trường Trung Quốc"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/m445spS5ZuzWvWGKgC35jjsHerM1-u4AOoKFvnCtl1I/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6446661_cover-tren-tay-cooler-master-orb-x-computex23-tinhte.jpg",
+      title: "Computex 2023: Trên tay Cooler Master ORB X - Trạm công nghệ đa năng"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/QMlQjfFrHQjUfulHKY-i_QJefWiku3Hi8V5H5Y5a8PU/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6462294_cover-Ban-Phim-VisionOS-tinhte.jpg",
+      title: "Đây là cách bàn phím ảo của visionOS phản hồi khi người dùng gõ"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/qDDmXq381rnYjsFpeeXf4wf7Rcswi9KUDLWw_4UVvwk/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6460277_Cover-Facetime-hoat-dong-nhu-the-nao-tren-vision-pro-tinhte-tuanhtran.jpg",
+      title: "FaceTime trên Apple Vision Pro hoạt động như thế nào?"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/QMlQjfFrHQjUfulHKY-i_QJefWiku3Hi8V5H5Y5a8PU/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6462294_cover-Ban-Phim-VisionOS-tinhte.jpg",
+      title: "Đây là cách bàn phím ảo của visionOS phản hồi khi người dùng gõ"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/m445spS5ZuzWvWGKgC35jjsHerM1-u4AOoKFvnCtl1I/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6446661_cover-tren-tay-cooler-master-orb-x-computex23-tinhte.jpg",
+      title: "Computex 2023: Trên tay Cooler Master ORB X - Trạm công nghệ đa năng"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/qDDmXq381rnYjsFpeeXf4wf7Rcswi9KUDLWw_4UVvwk/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6460277_Cover-Facetime-hoat-dong-nhu-the-nao-tren-vision-pro-tinhte-tuanhtran.jpg",
+      title: "FaceTime trên Apple Vision Pro hoạt động như thế nào?"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/QMlQjfFrHQjUfulHKY-i_QJefWiku3Hi8V5H5Y5a8PU/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6462294_cover-Ban-Phim-VisionOS-tinhte.jpg",
+      title: "Đây là cách bàn phím ảo của visionOS phản hồi khi người dùng gõ"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/m445spS5ZuzWvWGKgC35jjsHerM1-u4AOoKFvnCtl1I/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6446661_cover-tren-tay-cooler-master-orb-x-computex23-tinhte.jpg",
+      title: "Computex 2023: Trên tay Cooler Master ORB X - Trạm công nghệ đa năng"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/m445spS5ZuzWvWGKgC35jjsHerM1-u4AOoKFvnCtl1I/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6446661_cover-tren-tay-cooler-master-orb-x-computex23-tinhte.jpg",
+      title: "8 tính năng Apple đã “mượn” từ Android để mang vào iOS 17"
+    },
+    {
+      imgURL: "hhttps://imgproxy7.tinhte.vn/wHGR-h46Ki1FkrlHKsfISJaKdhUxSlHRmGfczBEjDM8/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6459974_cover-Tinh-Nang-iOS-17-Hoc-Android-tinhte.jpg",
+      title: "Computex 2023: Trên tay Cooler Master ORB X - Trạm công nghệ đa năng"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/m445spS5ZuzWvWGKgC35jjsHerM1-u4AOoKFvnCtl1I/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6446661_cover-tren-tay-cooler-master-orb-x-computex23-tinhte.jpg",
+      title: "Computex 2023: Trên tay Cooler Master ORB X - Trạm công nghệ đa năng"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/qDDmXq381rnYjsFpeeXf4wf7Rcswi9KUDLWw_4UVvwk/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6460277_Cover-Facetime-hoat-dong-nhu-the-nao-tren-vision-pro-tinhte-tuanhtran.jpg",
+      title: "FaceTime trên Apple Vision Pro hoạt động như thế nào?"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/m445spS5ZuzWvWGKgC35jjsHerM1-u4AOoKFvnCtl1I/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6446661_cover-tren-tay-cooler-master-orb-x-computex23-tinhte.jpg",
+      title: "Computex 2023: Trên tay Cooler Master ORB X - Trạm công nghệ đa năng"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/m445spS5ZuzWvWGKgC35jjsHerM1-u4AOoKFvnCtl1I/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6446661_cover-tren-tay-cooler-master-orb-x-computex23-tinhte.jpg",
+      title: "Computex 2023: Trên tay Cooler Master ORB X - Trạm công nghệ đa năng"
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/m445spS5ZuzWvWGKgC35jjsHerM1-u4AOoKFvnCtl1I/h:320/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6446661_cover-tren-tay-cooler-master-orb-x-computex23-tinhte.jpg",
+      title: "Computex 2023: Trên tay Cooler Master ORB X - Trạm công nghệ đa năng"
+    }
+  ]
 export const quickPost=[
 
 ]
+
+export const tempDataQuickPost = [
+    {
+      title: "WWDC 2023: Dự đoán những thay đổi lớn trên tính năng Apple Watch",
+      imgUrl: "https://imgproxy7.tinhte.vn/vcmtWk7uzaU4gccPM2fLQY15Icp5uI9h6ex7SU1YYKc/rs:fill:312:193:0/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6445871_watch-os-9-tinhte-cover-2.jpg",
+    },
+    {
+      title: "Hướng dẫn lưu sự kiện thành mã QR để in lên thiệp mời giúp tăng lượng khách tham dự và tăng thu nhập",
+      imgUrl: "https://imgproxy7.tinhte.vn/du6kMqk38D1SOlkuWqG57shTZPdTXkW-kNUrZUebXbM/rs:fill:312:193:0/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6462879_tinhte-qr-code.jpg",
+    },
+    {
+      title: "Google Photos bổ sung các công cụ chỉnh sửa nâng cao trên nền tảng web",
+      imgUrl: "https://imgproxy7.tinhte.vn/Q8ZkujUdfPTwSfN57CSD1V5jLd6gvXhspiH0D7GIe0A/rs:fill:312:193:0/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6462960_cover-google-office.jpg",
+    },
+    {
+      title: "Apple đã âm thầm sửa lỗi bỏ dấu tiếng Việt khi gõ tắt trên iOS 17 và iPadOS 17",
+      imgUrl: "https://imgproxy7.tinhte.vn/UefSpeuejn5W-NF7eG3tuJxyHI4NxJ7XDxRq0OnhwEQ/rs:fill:312:193:0/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6462622_Cover-Apple-sua-loi-ban-phim-iOS-iPadOS-macOS-tinhte.jpg",
+    },
+    {
+      title: "Xiaomi 13 Ultra ra mắt thị trường Châu Âu với mức giá 1.499 Euro",
+      imgUrl: "https://imgproxy7.tinhte.vn/rhFTshPhbcj9mOsBsFfUPT90-tdeBdL8y74yTD7P1O8/rs:fill:312:193:0/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6462570_cover-Xiaomi-13-Ultra-Thi-Truong-Chau-Au-tinhte.jpg",
+    },
+    {
+      title: "Trên tay FSP DAGGER PRO 750W và MSI Z590 PRO tương thích chuẩn 12VO",
+      imgUrl: "https://imgproxy7.tinhte.vn/eu6BXMZCclTEGeaVSrJg-Hoy6bmdCFdC2WAEkK9HFzs/rs:fill:312:193:0/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6462594_cover-tren-tay-fsp-dagger-pro-12vo-msi-z590-tinhte.jpg",
+    },
+    {
+      title: "[Hỏi 1Ci Innovation] Anh em có mua kính Apple Vision Pro không?",
+      imgUrl: "https://imgproxy7.tinhte.vn/aJEjh5MHgEJpZnYd2PzTUxXcgtx_D2YipCkMPnUajEo/rs:fill:312:193:0/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6462842_Cover-hoi-tinh-te-mua-kinh-vision-pro-tinhte.png",
+    },
+    {
+      title: "Trải nghiệm màn hình di động Asus ProArt PA148CTV: linh hoạt, nhiều tính năng để sáng tạo nội dung",
+      imgUrl: "https://imgproxy7.tinhte.vn/dXdMB9Jy9Mk4Jr0Ix7OtaERJviqHC79L9Lt51loMjUQ/rs:fill:312:193:0/plain/https://photo2.tinhte.vn/data/attachment-files/2023/06/6462706_cover-man-hinh-di-dong-asus-pa148ctv.jpg",
+    },
+    {
+      title: "tvOS 17: Cải thiện âm thanh đối thoại trong các chương trình truyền hình và phim ảnh",
+      imgUrl: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6462195_Cover-Tvos-17-cai-thien-am-thanh-giong-noi-tinhte-tuanhtran.jpg",
+    }
+  ]
+  
+  export   const tempDataOutstandingPost = [
+    {
+      rank: "1",
+      title: "WWDC 2023: Dự đoán những thay đổi lớn trên tính năng Apple Watch",
+      author: "Ruby Lee",
+    },
+    {
+      rank: "2",
+      title: "Apple Watch và những thay đổi mới",
+      author: "Christine Ha",
+    },
+    {
+      rank: "3",
+      title: "Gã khổng lồ công nghệ và những giá trị cốt lõi",
+      author: "Ruby Lee",
+    },
+    {
+      rank: "4",
+      title: "Điện thoại không xài ốp lưng",
+      author: "Ruby Lee",
+    },
+    {
+      rank: "5",
+      title: "Samsung vs Apple - sự đối đầu giữa 2 đế chế",
+      author: "Blue",
+    },
+    {
+      rank: "6",
+      title: "Những bí ẩn công nghệ",
+      author: "Ruby Lee",
+    }
+  ]
+  
+  export  const tempDataThreadItem = [
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6462015_cover.jpg",
+      title: "Screen Distance - Tính năng nhỏ nhưng rất hay trên iOS 17",
+      content: "Apple chỉ nói nhanh về tính năng này trong sự kiện WWDC23, nhưng sau một thời gian sử dụng trên phiên bản dev beta thì đây có thể nói là tính năng mình cảm thấy thích nhất. Screen Distance cho thấy Apple hay như nào trong…",
+      author: "Lê Phú Khương",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6461792_IMG_3922.jpg",
+      title: "Các tính năng âm thanh, giải trí và trung tâm gia đình mới sắp ra mắt.",
+      content: "Cải tiến hồ sơ người dùng: “Bạn có thể chọn ngôn ngữ và cài đặt của AirPods được ghép đôi cho từng người dùng. Và nếu bạn đánh thức Apple TV bằng điều khiển từ xa qua Trung tâm điều khiển trên iPhone, hồ sơ của bạn sẽ tự…",
+      author: "Liem Luong Duy",
+    },
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/xacq4TZLarFBzsyx4f37F1MIUK4k6nTHPTUuQqE87Z0/h:460/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6446077_tinhte-cover-sac.jpg",
+      title: "Mua thử 2 cục sạc Retro 35W và 67W ở Việt Nam - Thiết kế hoài cổ, nhỏ gọn - giá còn hơi cao",
+      content: "Video Unbox và thiết kế của Shargeek 35W & 67W Thiết kế - điểm đặc biệt trên cả 2 mẫu sạc Retro Cách đóng gói của cả 2 chiếc sạc đem đến ngay sự hoài cổ, cục sạc 35W Retro với hình ảnh của chiếc Apple Macintosh đời đầu…",
+      author: "Lab Review",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6461982_pride-month-tinhte.jpg",
+      title: "Pride Month: Tháng Sáu của cộng đồng LGBT",
+      content: "Theo trang nationaltoday, Bill Clinton là tổng thống Mỹ đầu tiên chính thức thừa nhận Pride Month vào năm 1999 và 2000. Năm 2009 đến 2016, Barack Obama tuyên bố Pride Month dành cho LGBT vào tháng Sáu. Tháng 5/2019, Donald…",
+      author: "Nghia Nguyen(boygia)",
+    }
+  ]
+  
+  
+  export   const tempDataReel = [
+    {
+      imgURL: "https://imgproxy7.tinhte.vn/dOLn-d_19yHj5h1G2l8CHV-aURqq6Ygi87Yk8qMYeXA/w:400/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6443237_image.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463094_image.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463095_AAPL_2023-06-13_11-05-16.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463085_image.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463087_b5c500625d466fda76e9657f1f1d1571.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463080_image.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463061_image.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463054_image.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463048_IMG_0754.jpeg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463040_Zentalk-thumb-trai-nghiem-tren-asus-tuf-gaming-a-15-2023.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463033_5355cecaa2b2e8644ac8f8c7ea307434.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463029_image.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6462979_apple-music-08_1280x720-800-resize.jpg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6462990_IMG_5333.jpeg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6462999_IMG_5334.jpeg",
+    },
+    {
+      imgURL: "https://photo2.tinhte.vn/data/attachment-files/2023/06/6463010_image.jpg",
+    }
+  ]
+  
+  export  const tempDataTopPost = [
+    {
+      postImageURL: 'https://photo2.tinhte.vn/data/attachment-files/2023/06/6462415_Cover-LM.jpg',
+      title: '24h of Le Mans kỷ niệm 100 năm: Chiến thắng như phim của Ferrari, lật đổ sự thống trị của Toyota',
+      author: 'P.W',
+      content: 'Gọi cuộc đua kỷ niệm 100 năm của 24h of Le Mans năm nay là kịch tính, chắc chắn sẽ là một kết luận đầy thiếu sót. Nó cảm xúc, căng thẳng đến tận phút chót, và chỉ một sai lầm nhỏ cũng có thể khiến một đội đua dâng chiến  thắng cho đối thủ, cho dù sai lầm đó ở những giờ đầu tiên hay những phút.',
+    },
+    {
+      postImageURL: 'https://imgproxy7.tinhte.vn/hM3Nj7bfFnRSil8csMmNpOlZlM9hdWGDvMDlm7Z3bWw/h:460/plain/https://photo2.tinhte.vn/data/attachment-files/2023/05/6448008_image.jpg',
+      title: '5 cách để làm việc trơn tru giữa các thiết bị Apple',
+      author: 'Luong Duy Liem',
+      content: 'Mình có thể tự tạo dự án iMovie với những hình ảnh và video clip mình quay từ iPhone rồi trên máy Mac thì mình chỉ bật AirDrop và đặt thành Mọi người là mình sẽ thấy mọi video mà mình đã chia sẻ từ iPhone trong thư mục Tải về trên máy Mac.Và khi mình cần ký một tài liệu trên máy Mac? Mà mình thì lại đang có iPad',
+    },
+    {
+      postImageURL: 'https://photo2.tinhte.vn/data/attachment-files/2023/02/6342203_tinhte_cover_peng_deeptalk.jpg',
+      title: 'Những điều thú vị về chim cánh cụt',
+      author: 'Ng Minh Hằng',
+      content: 'Lúc còn bé, ước mơ của mình là được một lần chạm tay vào cái bụng tròn tròn ú ú của những bạn cánh cụt, và tới nay thì điều đó vẫn chưa thực hiện được. Tuy nhiên, vì sở thích đó nên mình đã gom góp được một số kiến thức thú vị về loài chim “không biết bay” cực kỳ đáng yêu này.',
+    }
+  ]
+  
